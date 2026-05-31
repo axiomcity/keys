9134b22d7344b51285c23dc0515a6cf241e5b7c0
David berger <david.ber0169@gmail.com>
2026-05-31 20:28:46 +0200

    git

--- AdminTools.ps1
+++ AdminTools.ps1
@@ -3,7 +3,7 @@
 # Init admintools
 cd $env:USERPROFILE
 # Winget - https://learn.microsoft.com/en-us/windows/package-manager/winget/
-Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
 # Git - winget install --id Git.Git -e --source winget 
 winget install --id Git.Git -e --source winget
 # Github repo keys
@@ -12,5 +12,4 @@ cd "$env:USERPROFILE\keys"
 # Powershell 7 - https://learn.microsoft.com/fr-fr/powershell/scripting/install/install-powershell-on-windows?view=powershell-7.6
 iwr https://github.com/PowerShell/PowerShell/releases/download/v7.6.2/PowerShell-7.6.2-win-x64.msi -OutFile .\admintools\PS7.msi
 msiexec /i ./admintools/PS7.msi /quiet /norestar
-.\admintools\pwsh\AdminTools.ps1
-
+.\admintools\pwsh\ProfileUpdate.ps1
