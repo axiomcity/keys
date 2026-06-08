f03fa2dee21051d20e369e6b9749342f4e5e1f50
david <67384207+axiomcity@users.noreply.github.com>
2026-06-08 22:40:22 +0200

    Delete admintools.sh

--- admintools.sh
+++
@@ -1,7 +0,0 @@
-# ZELLIJ
-wget https://github.com/zellij-org/zellij/releases/download/v0.41.2/zellij-x86_64-unknown-linux-musl.tar.gz
-tar -xvf zellij-x86_64-unknown-linux-musl.tar.gz && chmod +x zellij && mv zellij /usr/bin
-# NEOFETCH
-curl -LO https://raw.githubusercontent.com/dylanaraps/neofetch/refs/heads/master/neofetch
-chmod +x neofetch && mv neofetch /usr/bin/"
-
